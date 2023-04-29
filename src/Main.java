249a0c0483cffd404d35733b9c04891d0a30462c
Syat <titmouse99@mail.ru>
2023-04-29 18:08:15 +0300

    1 Задача

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,43 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         task1();
+        question();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
-        int [] weight = new int [3];
+
+        int[] weight = new int[3];
         weight[0] = 1;
         weight[1] = 2;
         weight[2] = 3;
         System.out.println(weight[0]);
         System.out.println(weight[1]);
         System.out.println(weight[2]);
-        double [] weight1 = {1.57, 7.654, 9.986};
+        System.out.println();
+
+        double[] weight1 = {1.57, 7.654, 9.986};
         System.out.println(weight1[0]);
         System.out.println(weight1[1]);
         System.out.println(weight1[2]);
-        int [][] twoDimArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 4; j++) {
-                    System.out.print(" " + twoDimArray[i][j] + " ");
-                }
-                System.out.println();
-            }
-    }
-        // вроде выполнил задачу =)
+        System.out.println();
 
+        int[] box = {-2, -1, 0, 1, 2};
+        for (int i =0; i <= 4; i++) {
+            System.out.println(box[i]);
+        }
+    }
+
+    public static void question() {
+        System.out.println("Вопрос");
+        int[][] twoDimArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(+ twoDimArray[i][j] + ", ");
+            }
+        }
+    }
+    //как в таком случае убрать запятую в конце и как поменять порядок вывода (что бы было задом на перед)
 }
